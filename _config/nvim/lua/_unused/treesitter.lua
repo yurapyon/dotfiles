1970f7a2f9ee2df64c44597d55b12dd1982b141f
parcel <genericpb@gmail.com>
2026-03-27 08:22:06 -0500

    Stuff

--- _config/nvim/lua/_unused/treesitter.lua
+++ _config/nvim/lua/_unused/treesitter.lua
@@ -6,10 +6,10 @@ return {
     -- install_dir = vim.fn.stdpath("data") .. "/site",
     ensure_installed = { "rust", "toml" },
     auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
+    -- highlight = {
+    -- enable = true,
+    -- additional_vim_regex_highlighting = false,
+    -- },
     ident = { enable = true },
     rainbow = {
       enable = true,
